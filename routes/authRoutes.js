182b3134a1412144ac721998226c7e4df0e4b89c
mohammed Noufal M <mnoufal336@gmail.com>
2025-12-15 15:23:35 +0530

    first commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,61 @@
+import express from "express";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, mobile, password } = req.body;
+
+    if (!name || !email || !mobile || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = new User({ name, email, mobile, password });
+    await user.save();
+
+    res.status(201).json({ message: "Signup successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email ID" });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Wrong password" });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+export default router;
---
+++ routes/productRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import Product from "../models/Product.js";
+
+const router = express.Router();
+
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+export default router;
