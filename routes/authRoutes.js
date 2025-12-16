9e2d11869b502b899035c6ba4daca4e2bca90348
mohammed Noufal M <mnoufal336@gmail.com>
2025-12-16 14:08:51 +0530

    Fix authroutes filename for Linux

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 /* ================= SIGNUP ================= */
 router.post("/signup", async (req, res) => {
-  console.log("SIGNUP HIT:", req.body);
+ console.log("SIGNUP DATA:", req.body);
   try {
     const { name, email, password, mobile } = req.body;
 
